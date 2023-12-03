7a55a6553c030609ce1dedab888550140caafedf
parv-nangalia <nangaliaparv@gmail.com>
2023-12-03 13:33:10 +0530

    Sellpage & Cartpage

---
+++ src/components/Cartpage/Cartpage.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect } from "react";
+import { all } from "../../constants/icons";
+import { Itemdetails, Collapse } from "../../components";
+import { useNavigate } from "react-router-dom";
+
+import "./Cartpage.css";
+
+const Cartpage = ({ cart }) => {
+  //const [clickedDiv, setclickedDiv] = useState(null);
+  //const [form, setForm] = useState(false);
+  const navigate = useNavigate();
+
+  const [inputValues, setInputValues] = useState({});
+
+  const [formValues, setFormValues] = useState({});
+
+  const handleFormSubmit = (index, formData) => {
+    setFormValues((prevFormValues) => {
+      return {
+        ...prevFormValues,
+        [index]: {
+          ...(prevFormValues[index] || {}), // Preserve existing data for the index
+          ...formData,
+        },
+      };
+    });
+
+    console.log(formValues);
+  };
+
+  const setCart = (car) => {
+    car.forEach((item) => {
+      item["count"] = 1;
+    });
+    console.log("inserting count", car);
+
+    return car;
+  };
+
+  const setorderCart = (props) => {
+    localStorage.setItem("ordercart", props);
+  };
+
+  const initialCart = setCart([...cart]);
+
+  const [modifiedcart, setModifiedCart] = useState(initialCart);
+
+  useEffect(() => {
+    setModifiedCart(initialCart);
+  }, [cart]);
+
+  console.log(modifiedcart);
+
+  const increment = (prop) => {
+    setModifiedCart((prevCart) =>
+      prevCart.map((item) =>
+        item.key === prop.key ? { ...item, count: item.count + 1 } : item
+      ),
+    );
+      console.log("increment", prop, modifiedcart, cart);
+  };
+
+  const decrement = (prop) => {
+    setModifiedCart((prevCart) =>
+      prevCart.map((item) =>
+        item.key === prop.key
+          ? { ...item, count: Math.max(item.count - 1, 1) }
+          : item,
+      ),
+    );
+  };
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      const initialInputValues = {};
+      //const itemcount = {};
+      modifiedcart.forEach((item) => {
+        //itemcount[item.key] = item.count;
+
+        const keys = Array.from({ length: item.count }, (_, index) => index);
+        keys.forEach((key) => {
+          initialInputValues[item.key] = {
+            ...initialInputValues[item.key],
+            [key]: {
+              Age: 0,
+              brand: "No Brand",
+              Condition: "Unsure",
+            },
+          };
+        });
+      });
+      setInputValues(initialInputValues);
+      //setCount(itemcount);
+      //console.log("itemcount", itemcount);
+      console.log("initial", initialInputValues);
+      console.log("cart", cart);
+      console.log("modified", modifiedcart);
+    }
+  }, [modifiedcart]);
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const jsonData = JSON.stringify(formValues);
+    console.log(jsonData);
+    console.log("Form submitted");
+    //window.location.href = "/Order";
+    navigate("/Order");
+    console.log("hi");
+    setorderCart(jsonData);
+    console.log("setorderCart called");
+    const storedData = localStorage.getItem("ordercart");
+
+    // Parse the JSON data if it's stored as a JSON string
+    const parsedData = JSON.parse(storedData);
+
+    // Use the retrieved data as needed
+    console.log(parsedData);
+  };
+
+  return (
+    <div className="app__cart-main">
+      <h2>Cart</h2>
+      <ul>
+        <form onSubmit={submitForm}>
+          {modifiedcart.map((item, index) => (
+            <li key={index}>
+              <div className="app__cart-item">
+                <div className="app__cart-header">
+                  {all[item.key] &&
+                    React.createElement(all[item.key], {
+                      className: "app__cart-img",
+                    })}
+                  <div className="app__cart-desc">{item.key}</div>
+                  <div className="app__cart-itemcount">
+                    <button type="button" onClick={() => decrement(item)}>
+                      {" "}
+                      -{" "}
+                    </button>
+                    <span>{item.count}</span>
+                    <button type="button" onClick={() => increment(item)}>
+                      {" "}
+                      +{" "}
+                    </button>
+                  </div>
+                </div>
+                {Array.from({ length: item.count }).map((_, i) => (
+                  <div
+                    className="app__cart-item-collapse"
+                    key={`${item.key}-${i}`}
+                  >
+                    <Collapse
+                      component={
+                        <Itemdetails
+                          index={index}
+                          item={item.key}
+                          //savedetails={() => savedetails(index)}.  onClick={decrement(item.key)}
+                          // displayForm={displayform}               onClick={increment(item.key)}
+                          inputValues={inputValues[item.key]}
+                          onFormSubmit={handleFormSubmit}
+                          num={i}
+                        />
+                      }
+                    />
+                  </div>
+                ))}
+              </div>
+            </li>
+          ))}
+          <button type="submit">Submit</button>
+        </form>
+      </ul>
+    </div>
+  );
+};
+
+export default Cartpage;
---
+++ src/components/Sellpage/Sellpage.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+// import { GiLaptop } from "react-icons/gi";
+import {
+  commonIcons,
+  wearableIcons,
+  householdIcons,
+  miscIcons,
+} from "../../constants/icons";
+
+import "./Sellpage.css";
+
+function ShowGadgets({ items, addToCart }) {
+  return (
+    <div className="app__sell-items">
+      {Object.keys(items).map((key) => {
+        const IconComponent = items[key]; // Get the icon component for the key
+        // console.log(`Key: ${key}`);
+        // console.log("Icon Component:", IconComponent);
+        return (
+          <div className="app__sell-icons" onClick={() => addToCart({ key })}>
+            <IconComponent className="app__sell-icon" />
+            <p>{key}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const list = [
+  {
+    id: 1,
+    head: "Common Electronics",
+    items: commonIcons,
+  },
+  {
+    id: 2,
+    head: "Wearables",
+    items: wearableIcons,
+  },
+  {
+    id: 3,
+    head: "Household/Offices",
+    items: householdIcons,
+  },
+  {
+    id: 4,
+    head: "Miscellaneous",
+    items: miscIcons,
+  },
+];
+
+const Sellpage = ({ cart, updateCart }) => {
+  const [expandedList, setExpandedList] = useState(null);
+
+  const expandlist = (items) => {
+    setExpandedList(items);
+  };
+
+  const addToCart = (product) => {
+    //console.log(cart);
+    //console.log(product);
+    //console.log(IconComponent);
+    //const obj = {key : product, Component: IconComponent};
+    if(cart.some(item => item.key === product.key)){
+      alert("item alreadye xists")
+    }
+    else{
+      const update = [...cart,product];
+      updateCart(update);
+    }
+  };
+
+  return (
+    <div className="app__sell-component">
+      <div className="app__sell-head">
+        <h2>
+          Add all the categories which you want to sell
+        </h2>
+      </div><br />
+      <div className="app__sell-list">
+        <ul>
+          {list.map((type) => (
+            <div
+              className="app__sell-type"
+              key={type.id}
+              onClick={() => expandlist(type.items)}
+            >
+              <h2>{type.head}</h2>
+            </div>
+          ))}
+        </ul>
+      </div>
+      {expandedList && (
+        <ShowGadgets items={expandedList} addToCart={addToCart} />
+      )}
+    </div>
+  );
+};
+
+export default Sellpage;
