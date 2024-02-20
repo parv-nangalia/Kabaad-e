12c201c09c0cc6a3dbebf42f2c439c7036d2d2de
parv-nangalia <nangaliaparv@gmail.com>
2024-02-20 15:26:11 +0530

    Fixing media redundancy

---
+++ src/constants/images.js
@@ -0,0 +1,21 @@
+import bg from "../assets/bg.png";
+
+import logo from "../assets/Logo_kabaad-e.png";
+import overlaybg from "../assets/overlaybg.png";
+import spoon from "../assets/spoon.svg";
+import header_img from "../assets/old-tech.jpg";
+import cart_illus from "../assets/cart-illus.png";
+import pickup from "../assets/pickup.png";
+import wallet from "../assets/wallet.png";
+
+
+export default {
+  bg,
+  logo,
+  overlaybg,
+  spoon,
+  header_img,
+  cart_illus,
+  pickup,
+  wallet,
+};
--- src/container/index.js
+++ src/container/index.js
@@ -1,21 +1,15 @@
 import AboutUs from "./AboutUs/AboutUs";
 import Start from "./Start/Start";
+import CaroHome from "./Start/Carousel";
 import FindUs from "./Findus/FindUs";
 import Footer from "./Footer/Footer";
-import Gallery from "./Gallery/Gallery";
 import Header from "./Header/Header";
-import Intro from "./Intro/Intro";
-import Laurels from "./Laurels/Laurels";
-import SpecialMenu from "./Menu/SpecialMenu";
 
 export {
   AboutUs,
   Start,
+  CaroHome,
   FindUs,
   Footer,
-  Gallery,
   Header,
-  Intro,
-  Laurels,
-  SpecialMenu
 };
